49d5bd01078d0aa8687f8f339c916a8aa2df4d2b
Charles Cochrane <cochrane26@gmail.com>
2015-07-01 02:10:45 +0100

    added serial and parrallel requests

--- exampleBackEnd/main.go
+++ exampleBackEnd/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("hit")
+	time.Sleep(time.Second)
 	fmt.Fprint(w, "Hi there")
 }
 
--- proxyServer.go
+++ proxyServer.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
@@ -15,13 +16,26 @@ var proxyCalls int
 func main() {
 
 	router := httprouter.New()
-	router.GET("/proxy", reverseProxy)
+	router.GET("/test1", reverseProxy)
+	router.GET("/test2", serialCompositionProxy)
+	router.GET("/test3", parrallelCompositionProxy)
 	router.GET("/stats", stats)
 
 	http.Handle("/", router)
 
 	log.Println("Listening...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	//Instead of using http.listen... you can create a custom server to create custom behavior
+	//such as read and write timeouts or max header bytes:
+	// s := &http.Server{
+	// 	Addr:           ":8080",
+	// 	Handler:        router,
+	// 	ReadTimeout:    10 * time.Second,
+	// 	WriteTimeout:   10 * time.Second,
+	// 	MaxHeaderBytes: 1 << 20,
+	// }
+	// log.Fatal(s.ListenAndServe())
 }
 
 func reverseProxy(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
@@ -37,6 +51,55 @@ func reverseProxy(res http.ResponseWriter, req *http.Request, _ httprouter.Param
 	log.Println(req.URL)
 	req.URL.Path = "/"
 	proxy.ServeHTTP(res, req)
+}
+
+func serialCompositionProxy(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	body := make([]string, 4)
+
+	for i := 0; i < 4; i++ {
+		resp, err := http.Get("http://localhost:3000")
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		str, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+
+		body[i] = string(str)
+	}
+
+	fmt.Fprintln(res, body)
+}
+
+func parrallelCompositionProxy(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	body := make([]string, 4)
+	ret := make(chan string)
+
+	for i := 0; i < 4; i++ {
+		go func() {
+			resp, err := http.Get("http://localhost:3000")
+			if err != nil {
+				panic(err)
+			}
+
+			str, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				panic(err)
+			}
+
+			resp.Body.Close()
+			ret <- string(str)
+		}()
+	}
+
+	for returns := 0; returns < 4; {
+		body[returns] = <-ret
+		returns++
+	}
+	fmt.Fprintln(res, body)
 }
 
 func stats(res http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
